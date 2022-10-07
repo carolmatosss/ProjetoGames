ea8fd53f78a09689ee933b849750aeea670abfc5
carolmatosss <carolyne_nascimento@hotmail.com>
2022-10-07 16:32:19 -0300

    Atualizando atividade

--- src/main/java/com/games/games/repository/CategoriaRepository.java
+++ src/main/java/com/games/games/repository/CategoriaRepository.java
@@ -4,9 +4,10 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import com.games.games.model.CategoriaModel;
-
+@Repository
 public interface CategoriaRepository extends JpaRepository<CategoriaModel , Long>{
 	
 	public List<CategoriaModel> findAllByTemaContainingIgnoreCase(@Param ("tema") String tema);
--- src/main/java/com/games/games/repository/ProdutoRepository.java
+++ src/main/java/com/games/games/repository/ProdutoRepository.java
@@ -4,11 +4,12 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import com.games.games.model.ProdutoModel;
 
 //Interface vai extender o JPA 
-
+@Repository
 public interface ProdutoRepository extends JpaRepository <ProdutoModel, Long> {
 	public List<ProdutoModel> findAllByTituloContainingIgnoreCase(@Param ("titulo") String titulo); 
 	public List<ProdutoModel> findAllByMarcaContainingIgnoreCase(@Param ("marca") String marca);
